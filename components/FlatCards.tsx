fa0829e242b15bee79db7ba7680dbdccadf3be13
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2024-03-17 18:11:13 +0530

    done

---
+++ components/FlatCards.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>FlatCards</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal:10
+
+    }
+})
